466364c419956a7f54a910e01a4ffb99ce5d0990
grbcp5 <grbcp5@mst.edu>
2017-12-14 17:06:04 -0600

    Create Error codes module

---
+++ Error.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *
+ * File:
+ *   Error
+ *
+ * Author:
+ *   Grant Broadwater
+ *
+ * Project:
+ *   maxwell
+ *
+ ******************************************************************************/
+
+#ifndef MAXWELL_ERROR_H
+#define MAXWELL_ERROR_H
+
+enum Error {
+
+    /* ERR_NO_ERR = 0, */
+
+    /* 100s - Argument Errors */
+    ERR_UNEXPECTED_TOKEN = 115
+
+};
+
+#endif //MAXWELL_ERROR_H

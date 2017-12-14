9f7cf69ffe4a7d93901755a68ae12432a20cdd55
grbcp5 <grbcp5@mst.edu>
2017-12-14 16:48:51 -0600

    Add primitive type definitions

---
+++ Types.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *
+ * File:
+ *   Types
+ *
+ * Author:
+ *   Grant Broadwater
+ *
+ * Project:
+ *   maxwell
+ *
+ ******************************************************************************/
+
+#ifndef MAXWELL_TYPES_H
+#define MAXWELL_TYPES_H
+
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned int dword;
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
+
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
+
+
+#endif // MAXWELL_TYPES_H
